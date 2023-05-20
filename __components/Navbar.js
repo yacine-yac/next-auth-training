aee9ce6a19d3b947b86d7f4f30eb66ccdec92466
yacine <lentente1958@gmail.com>
2023-05-20 22:26:13 +0100

    start auth blog app

---
+++ __components/Navbar.js
@@ -0,0 +1,9 @@
+import style from "./nav.module.css"
+function Navbar(){
+    return <>
+        <div className={style.nav}>
+            <button>SIgn</button>
+        </div>
+    </>
+}
+export default Navbar;
---
+++ pages/profil.js/index.js
@@ -0,0 +1,5 @@
+export default function Profil({name}){
+    return <>
+      <h1>Hello in your profil Mr: {name} </h1>
+    </>
+}
